bb7434ea1d58070c7865773775f738a59991145b
Kelsey Benjamin <kbenjamin4@pvamu.edu>
2024-08-10 18:10:37 -0500

    init

---
+++ src/main/kotlin/com/kchanel/tolltagsearch/TollTagSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.kchanel.tolltagsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TollTagSearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<TollTagSearchApplication>(*args)
+}
---
+++ src/test/kotlin/com/kchanel/tolltagsearch/TollTagSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kchanel.tolltagsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TollTagSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
