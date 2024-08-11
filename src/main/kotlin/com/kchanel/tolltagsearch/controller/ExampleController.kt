4f687b86d92436c3bda9d877640544c1c98ecd9f
Kelsey Benjamin <kbenjamin4@pvamu.edu>
2024-08-11 06:04:04 -0500

    Updated example apis and base url

--- src/main/kotlin/com/kchanel/tolltagsearch/controller/ExampleController.kt
+++ src/main/kotlin/com/kchanel/tolltagsearch/controller/ExampleController.kt
@@ -3,18 +3,20 @@ package com.kchanel.tolltagsearch.controller
 
 import com.kchanel.tolltagsearch.repository.ExampleRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/example")
 class ExampleController(private val exampleRepository: ExampleRepository) {
 
-    @GetMapping("/example")
+    @GetMapping("/profile")
     suspend fun example(): String {
         return "example using profile: " + exampleRepository.getProfile()
     }
 
-    @GetMapping("/example1")
+    @GetMapping("/demo")
     fun example1(): String {
-        return "example1"
+        return "example"
         }
 }
